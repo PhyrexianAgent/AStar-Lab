c85164a470ae06e78cb700637fc08c898bb1c875
PhyrexianAgent <aidanthewizard5@gmail.com>
2024-10-13 17:41:43 -0400

    Added Code for PriorityQueue

--- Assets/Scripts/PriorityQueue.cs
+++ Assets/Scripts/PriorityQueue.cs
@@ -1,18 +1,24 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class PriorityQueue : MonoBehaviour
+public class PriorityQueue
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private ArrayList nodes = new ArrayList();
+
+    public int Length() => nodes.Count;
+    public bool Contains(object node) => nodes.Contains(node);
+
+    public Node GetFirst(){
+        if (nodes.Count == 0)
+            return null;
+        return (Node)nodes[0];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Add(Node node){
+        nodes.Add(node);
+        nodes.Sort();
+    }
+
+    public void Remove(Node node){ // I am pretty sure removing a node from a list of nodes will keep same order so sorting it is pointless
+        nodes.Remove(node);
     }
 }
